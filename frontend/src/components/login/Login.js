4f2e00c8f5cf67406fe89fc16c3a4e839cdf3132
Kabindra Shrestha <kabindra.shrestha.0wn@gmail.com>
2020-06-09 16:43:40 +0545

    changed browse router to hash router

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import UtilsToast from "../utils/UtilsToast";
-import {Button, Card, Col, Form} from "react-bootstrap";
+import {Button, Card, Col, Container, Form, Row} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faSave, faUser} from '@fortawesome/free-solid-svg-icons';
 
@@ -63,50 +63,56 @@ export default class Login extends Component {
         const {username, password} = this.state;
 
         return (
-            <div>
-                <div style={{"display": this.state.show ? "block" : "none"}}>
-                    <UtilsToast show={this.state.show}
-                                message={this.state.message}
-                                type={this.state.status ? "success" : "error"}/>
-                </div>
-                <Card>
-                    <Card.Header><FontAwesomeIcon
-                        icon={faUser}/> Login</Card.Header>
-                    <Form id="userFormId" onSubmit={this.submitUser}>
-                        <Card.Body>
-                            <Form.Row>
-                                <Form.Group as={Col} controlId="formGridUsername">
-                                    <Form.Label>Username</Form.Label>
-                                    <Form.Control
-                                        required autoComplete="off"
-                                        type="text" name="username"
-                                        value={username}
-                                        onChange={this.userChange}
-                                        className=""
-                                        placeholder="Enter Username"/>
-                                </Form.Group>
-                            </Form.Row>
-                            <Form.Row>
-                                <Form.Group as={Col} controlId="formGridPassword">
-                                    <Form.Label>Password</Form.Label>
-                                    <Form.Control
-                                        required autoComplete="off"
-                                        type="password" name="password"
-                                        value={password}
-                                        onChange={this.userChange}
-                                        className=""
-                                        placeholder="Enter Password"/>
-                                </Form.Group>
-                            </Form.Row>
-                        </Card.Body>
-                        <Card.Footer style={{"textAlign": "right"}}>
-                            <Button size="sm" variant="success" type="submit">
-                                <FontAwesomeIcon icon={faSave}/> Login
-                            </Button>
-                        </Card.Footer>
-                    </Form>
-                </Card>
-            </div>
+            <Container>
+                <Row>
+                    <Col lg={12}>
+                        <div>
+                            <div style={{"display": this.state.show ? "block" : "none"}}>
+                                <UtilsToast show={this.state.show}
+                                            message={this.state.message}
+                                            type={this.state.status ? "success" : "error"}/>
+                            </div>
+                            <Card>
+                                <Card.Header><FontAwesomeIcon
+                                    icon={faUser}/> Login</Card.Header>
+                                <Form id="userFormId" onSubmit={this.submitUser}>
+                                    <Card.Body>
+                                        <Form.Row>
+                                            <Form.Group as={Col} controlId="formGridUsername">
+                                                <Form.Label>Username</Form.Label>
+                                                <Form.Control
+                                                    required autoComplete="off"
+                                                    type="text" name="username"
+                                                    value={username}
+                                                    onChange={this.userChange}
+                                                    className=""
+                                                    placeholder="Enter Username"/>
+                                            </Form.Group>
+                                        </Form.Row>
+                                        <Form.Row>
+                                            <Form.Group as={Col} controlId="formGridPassword">
+                                                <Form.Label>Password</Form.Label>
+                                                <Form.Control
+                                                    required autoComplete="off"
+                                                    type="password" name="password"
+                                                    value={password}
+                                                    onChange={this.userChange}
+                                                    className=""
+                                                    placeholder="Enter Password"/>
+                                            </Form.Group>
+                                        </Form.Row>
+                                    </Card.Body>
+                                    <Card.Footer style={{"textAlign": "right"}}>
+                                        <Button size="sm" variant="success" type="submit">
+                                            <FontAwesomeIcon icon={faSave}/> Login
+                                        </Button>
+                                    </Card.Footer>
+                                </Form>
+                            </Card>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
